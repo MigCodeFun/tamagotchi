f16ed2fd3cb2b98e7f5dec004f4ac2eb1325e55a
migcode <ribeiro.igor@hotmail.fr>
2024-01-11 15:44:29 +0100

    Add Tamastudi states and actions to script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+États de notre Tamastudi possibles :
+- 🥚 : partie non lancée
+- 🐣 : naissance pendant tant qu'il n'a pas fait son 1er caca
+Ensuite il devient un "grand" avec une humeur variable
+- 😢 : triste 0/5
+- 🙁 : pas content 1/5
+- 🙂 : normal 2/5
+- 😄 : content 3/5
+- 🤗 : heureux 4/5
+- 🥰 : très heureux 5/5
+- 👻 : mort 0/5
+Ses envies :
+- 😋 : faim, aléatoire minimum 30 sec et max 3 minutes
+- 🥱 : jouer, aléatoire minimum 30 sec et max 3 minutes
+- 💩 : caca, aléatoire minimum 30 sec et max 1.30 minutes uniquement après avoir mangé
+*/
+
+
+/*PEMIERE FONVTION:demander le prénom*/
+const tamaName= prompt("Quel est le prénom de votre Tamastudi ?")
+console.log(tamaName);
